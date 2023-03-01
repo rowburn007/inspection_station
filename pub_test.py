a7c2ebc1d209f000cc0d42299ed9ecaf686212e5
rowen <rowendburney@gmail.com>
2023-02-28 21:45:16 -0500

    kivy implementation started

--- pub_test.py
+++ pub_test.py
@@ -1,40 +1,4 @@
-# import paho.mqtt.client as mqtt
-# import time
-#
-# def on_log(client, userdata, level, buf):
-#     print(f"log: {buf}")
-#
-# def on_connect(client, userdata, flags, rc):
-#     if rc == 0:
-#         print("Connected!")
-#     else:
-#         print(f"Bad connection returned code = {rc}")
-#
-# def on_disconnect(client, userdata, flags, rc=0):
-#     print(f"Disconnected, result code = {str(rc)}")
-#
-#
-# broker= "test.mosquitto.org"
-# port = 1883
-#
-# client = mqtt.Client("Classification-Station") # Creating new instance
-# client.on_connect = on_connect
-# # client.on_log = on_log
-# client.on_disconnect = on_disconnect
-#
-#
-# print(f"Connecting to broker : {broker}")
-# client.connect(broker, port=port)  # Connects to broker
-#
-# client.loop_start()  # Start loop
-# client.publish("tray-detection", "detected")  # Publishes to topic tray-detection
-# time.sleep(4)
-# client.loop_stop()  # End loop
-#
-# client.disconnect()  # Disconnects
-
 import paho.mqtt.client as mqtt
-
 
 def on_publish(client, userdata, message):
     print("published")
