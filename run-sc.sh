2a09fa9ad7eb73b55d6c73e72eee11c18b5d5a1e
rowen <rowendburney@gmail.com>
2023-02-28 22:32:51 -0500

    kivy implementation started

--- run-sc.sh
+++
@@ -1,4 +0,0 @@
-#!/system/bin/sh
-
-# Run a command as the root user
-su -c 'docker run -t tray-classifier'
---
+++ run-tc.sh
@@ -0,0 +1,5 @@
+#!/system/bin/sh
+
+su -c 'docker run -t tray-classifier'
+# Run a command as the root user
+#su -c 'docker run -t tray-classifier'
